81ee5139a0bfc5e18524d474816652902ef3fbc6
tuvaforrest <forrest@tuvahealth.com>
2024-03-06 18:32:19 -0500

    pointed procedure to normalized version

--- models/core/final/core__procedure.sql
+++ models/core/final/core__procedure.sql
@@ -3,20 +3,5 @@
    )
 }}
 
-{% if var('clinical_enabled', var('tuva_marts_enabled',False)) == true and var('claims_enabled', var('tuva_marts_enabled',False)) == true -%}
-
-select * from {{ ref('core__stg_claims_procedure') }}
-union all
-select * from {{ ref('core__stg_clinical_procedure') }}
-
-{% elif var('clinical_enabled', var('tuva_marts_enabled',False)) == true -%}
-
-select * from {{ ref('core__stg_clinical_procedure') }}
-
-{% elif var('claims_enabled', var('tuva_marts_enabled',False)) == true -%}
-
-select * from {{ ref('core__stg_claims_procedure') }}
-
-{%- endif %}
-
+select * From {{ref('core__procedure')}}
 
