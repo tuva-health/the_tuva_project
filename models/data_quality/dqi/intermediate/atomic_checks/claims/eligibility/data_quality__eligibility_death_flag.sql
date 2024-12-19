44ca975051e5c7cacba6dadd43897f6726444553
sarah-tuva <sarah@tuvahealth.com>
2024-12-19 09:35:28 -0700

    Fix type casting

--- models/data_quality/dqi/intermediate/atomic_checks/claims/eligibility/data_quality__eligibility_death_flag.sql
+++ models/data_quality/dqi/intermediate/atomic_checks/claims/eligibility/data_quality__eligibility_death_flag.sql
@@ -11,11 +11,11 @@ SELECT DISTINCT
     ,'ELIGIBILITY' AS claim_type
     ,'DEATH_FLAG' AS field_name
     ,case
-        when cast(m.death_flag as {{ dbt.type_string() }}) in ('1','0') then 'valid'
+        when cast(cast(m.death_flag as integer) as {{ dbt.type_string() }}) in ('1','0') then 'valid'
         when m.death_flag is null then 'null'
         else 'invalid'
         end as bucket_name
     ,cast(null as {{ dbt.type_string() }}) as invalid_reason
-    ,cast(death_flag as {{ dbt.type_string() }}) as field_value
+    ,cast(cast(death_flag as integer) as {{ dbt.type_string() }}) as field_value
     , '{{ var('tuva_last_run')}}' as tuva_last_run
 from {{ ref('eligibility')}} m
